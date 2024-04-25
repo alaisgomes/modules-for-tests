3f890463b4971fddb077893073e969f440f1d5bb
alaisgomes <alaisgomes@gmail.com>
2024-04-25 12:15:08 -0400

    Adding initial modules and code

---
+++ modules/django-simple-django/django_simple_django/simple_django/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SimpleDjangoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'modules.django_simple_django.simple_django'
---
+++ modules/react-native-simple-template/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function SimpleTemplate() {
+  return (
+    <View>
+      <Text>simple template</Text>
+    </View>
+  );
+}
+
+export default {
+  title: "simple-template",
+  navigator: SimpleTemplate,
+};
